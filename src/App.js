1eb0aac8e272ecf7b0a1cf0ea06bce0634d20d01
kaoxiong20 <kaoxiong520@gmail.com>
2023-04-25 19:46:54 -0500

    Added a Home, About, and NavBar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Home from '.src/components/Home';
+import NavBar from '.src/components/NavBar';
+import About from '.src/components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NavBar/>
+      <About/>
+      <Home/>
     </div>
   );
 }
+
+
+
+
+
+
+
+//   return (
+//     <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Home = () => {
+
+  return (
+    <>
+    <h1>Hello.</h1></>
+  );
+}
+
+export default Home;
