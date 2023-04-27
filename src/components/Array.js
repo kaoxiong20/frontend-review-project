d94f838f4b9f7c359d289f654a5192392ab054ad
kaoxiong20 <kaoxiong520@gmail.com>
2023-04-27 17:14:08 -0500

    Created an Array component to house an array for the cards

---
+++ src/components/Array.js
@@ -0,0 +1,23 @@
+const Array = [
+    {
+      id: '1',
+      name: Home
+    },
+
+    {
+      id: '2',
+      name: About
+    },
+
+    {
+      id: '3',
+      name: Experience
+    },
+
+    {
+      id: '4',
+      name: Other
+    }
+  ];
+  
+  export default Array;
