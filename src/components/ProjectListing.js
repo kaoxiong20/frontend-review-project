17732b98a8451818bca9a24b1aaca47e22d5d958
kaoxiong20 <kaoxiong520@gmail.com>
2023-04-28 00:21:22 -0500

    Added a title to the Projects component

--- src/components/ProjectListing.js
+++ src/components/ProjectListing.js
@@ -16,7 +16,6 @@ export default ProjectListing;
 
 const Name = styled.p`
     font-size: 25px;
-    text-transform: uppercase;
     font-family: 'Unica One';
 `
 
@@ -27,7 +26,6 @@ const Type = styled.p`
 const StyledDiv = styled.div`
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
     transition: 0.3s;
-    width: 40%;
     padding: 10px;
     margin-bottom: 15px;
     background-color: #9CA89E;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ProjectsCard from "./ProjectsCard";
+import styled from "styled-components";
 
 function Projects () {
     return(
-        <ProjectsCard />
+        <>
+            <ProjectsTitle>My Projects</ProjectsTitle>
+            <ProjectsCard />
+        </>
     )
 };
 
 export default Projects;
+
+const ProjectsTitle = styled.h1`
+    font-family: 'Unica One';
+    font-size: 45px;
+    text-align: center;
+    margin-bottom: 0px;
+    margin-top: 50px;
+`
